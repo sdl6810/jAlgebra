59da3442d9d4859a495a0728295c8fbccc5f6842
sdl6810 <sdl5384@gmail.com>
2023-07-26 17:33:10 -0400

    Polynomial v1 class submitted.
    First generation of this code.
    It can only deconstruct a polynomial into a list of terms.  More developments coming.

--- Polynomial.java
+++ Polynomial.java
@@ -4,27 +4,35 @@ import java.lang.Math.*;
 public class Polynomial
 {
 	private ArrayList<String> terms = new ArrayList<String>();
+	private ArrayList<String> coefficients = new ArrayList<String>();
 
 	//set at zero until constructor will set it to a value using the getDegree() method
 	private int degree = 0;
-	private String[] types = ["constant","linear","quadratic","cubic","quartic","quintic"]
+	private String[] types = {"constant","linear","quadratic","cubic","quartic","quintic"};
 	private String kindOfPolynomial = null;
-	private String[] functionPrefixes = ["sin(", "cos(", "tan(", "ln(", "log(", "e(", "asin(", "acos(", "atan", "sqrt("]
+	private String[] functionPrefixes = {"sin(", "cos(", "tan(", "ln(", "log(", "exp(", "asin(", "acos(", "atan", "sqrt("};
+
+	Scanner keyboard = new Scanner(System.in);
 
 	public Polynomial()
 	{
+		//throw exception that if any function prefixes are contained in the given "polynomial" then Java should state this as such and have the user to try again
+		System.out.println("Enter your polynomial.");
+		System.out.println("Remember to use asterisk symbols for multiplication and forward slashes for division.");
+		System.out.println("Be sure to use parentheses whenever you can to clarify the operation as much as possible.");
 
+		String pText = keyboard.next();
+		this.deconstructPolynomial(pText);
 	}
 
-	public ArrayList<String> breakIntoTerms(String characters)
+	private void deconstructPolynomial(String characters)
 	{
-		ArrayList<String> substrings = new ArrayList<String>();
-
 		int currentSplitIndex = 0;
 		int previousSplitIndex = 0;
 		String result = null;
 		String lastTerm = null;
 
+		//identify and separate each of the terms as each element in the ArrayList
 		int i = 0;
 		int tempLocation = 0;
 		while (i < characters.length())
@@ -37,7 +45,7 @@ public class Polynomial
 					newSubstring.append(characters.charAt(j));
 					result = newSubstring.toString();
 				}
-			substrings.add(result);
+			this.terms.add(result);
 			tempLocation = i;
 			previousSplitIndex = tempLocation;
 			}
@@ -53,17 +61,110 @@ public class Polynomial
 			previousSplitIndex = tempLocation;
 			i++;
 		}
-		substrings.add(lastTerm);
-		return substrings;
+		this.terms.add(lastTerm);
 	}
 
-	public double identifyFunction(String functionInText)
+	/*private double reconstructAndEvaluate()
 	{
+		double result = 0;
+		for (int i = 0; i <= p.size()-1; i++)
+		{
+			if (this.terms.get(i))
+			for (int j = 0; j <= this.functionPrefixes.length-1; j++)
+			{
+				if (this.terms.get(i).contains(j))
+			}
+		}
+	}*/
 
+	//this method will just return the value at x.  User does not need to see inner workings of the private helper method
+	public double evaluate()
+	{
+		return 0;
 	}
 
-	public double evaluate(ArrayList<Double> values)
+	//place logic in here to determine the degree of the polynomial
+	private void setDegree()
 	{
-		
+		this.degree = 0;
+	}
+
+	private String getDegree()
+	{
+		return null;
+	}
+
+	private boolean isLinear()
+	{
+		if (this.degree == 1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	private boolean isQuadratic()
+	{
+		if (this.degree == 2)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	private boolean isCubic()
+	{
+		if (this.degree == 3)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	private boolean isQuartic()
+	{
+		if (this.degree == 4)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	private boolean isQuintic()
+	{
+		if (this.degree == 5)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/*private ArrayList<Double> extractCoefficients()
+	{
+		for (int i = 0; i <= this.coefficients.size()-1;i++)
+		{
+
+		}
+	}*/
+
+	public static void main(String[] args)
+	{
+		Polynomial p = new Polynomial();
+		System.out.println(p.terms);
 	}
 }
